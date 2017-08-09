2e2d49bed9bb3c7a6846739f5cddf713749b38c2
João Rodrigues <joao.rodrigues@securibox.eu>
2017-08-09 16:34:34 +0100

    corrected autoload

--- test/CloudAgentsTest.php
+++ test/CloudAgentsTest.php
@@ -8,9 +8,9 @@ use PHPUnit\Framework\TestCase;
 use Securibox;
 
 class CloudAgentsTest extends TestCase{
-    private $username = "AssoSynergie";
+    private $username = "%Basic_username%";
 
-    private $password = "vgy5DsJj2GXBuQ7D";
+    private $password = "%Basic_password%";
     public function testGetCategories(){
         $client = new Securibox\CloudAgents($this->username, $this->password);
         $resp = $client->getCategories();
